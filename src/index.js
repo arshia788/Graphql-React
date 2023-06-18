6ce19da48cf7d936b3f6e90c953292651261390f
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-18 20:30:43 +0330

    learning graphql

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+
+// ertebat ma ro miad bargharar mikoneh ba graphql 
+// ApolloClient ertebat ma ba backend graphql miad vasl mikoneh 
+const client = new ApolloClient({
+    uri: "https://graphqlzero.almansi.me/ap",
+
+    // * agar data gerefteh beshe dg niaz nabashe dobareh darkhast befrsteim
+    // * baray hamin miam yek ja save mikonim ta sg dar khast nafrestim
+    cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    // *akhar sar bayad in Apollo ro wrap bokoni door App
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
